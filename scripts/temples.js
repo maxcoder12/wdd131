bb5bea133144f584ef152f01b7a78cfb94015ba2
maxcoder12 <maxgblds@gmail.com>
2025-01-18 17:18:47 -0300

    finished

--- scripts/temples.js
+++ scripts/temples.js
@@ -35,4 +35,15 @@ smallLink.addEventListener("click", function () {
     currentSelection.textContent = smallLink.innerText;
 });
 
+const today = new Date();
 
+const currentYear = document.querySelector("#currentYear")
+const lastModified = document.querySelector("#lastModified")
+
+
+const fullYear = today.getFullYear()
+
+const lastModification = document.lastModified
+
+currentYear.textContent = fullYear
+lastModified.textContent = `Last Modification: ${lastModification}`
